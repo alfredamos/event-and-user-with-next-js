c6bebed4cc4e362d305b6d70879a097599fee0db
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2026-03-07 20:40:04 +0100

    Add proxy (middleware) for authentication.

---
+++ src/proxy.ts
@@ -0,0 +1,36 @@
+// proxy.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import {isPublicRoute} from "@/servers/utils/publicRoutes.util";
+import {authService} from "@/servers/services/auth.service";
+
+export default async function proxy(request: NextRequest) {
+    //----> Log the incoming request
+    console.log(`Incoming request: ${request.method} ${request.url}`);
+
+    //----> Public routes are allowed to access without authentication.
+    const isPublic = isPublicRoute(request);
+    if (isPublic) {
+        return NextResponse.next();
+    }
+
+    //----> Get user session.
+    const session = await authService.getUserSession();
+    console.log("In middleware, Session: ", session);
+    //----> If the user is not authenticated, redirect to the login page.
+    if (!session?.isLoggedIn) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    //----> Continue to the next step (route handler or page)
+    return NextResponse.next();
+}
+
+
+//----> The matcher config defines the paths where the middleware will be applied
+export const config = {
+    matcher: [
+        "/((?!^_next/static|_next/image|favicon.ico|login|signup|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css)$|^$|^/$).*)",
+    ],
+};
+
